fe4db158cf26810cda65cc80868c990c3fb68216
Ivan Latunov <ivan.latunov@chaosgroup.com>
2017-07-03 16:23:46 +0300

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+	entry: './index.jsx',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'spdx-to-react.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+				query: {
+					presets: [
+						'es2015',
+						'react'
+					],
+				}
+			}
+		]
+	},
+	externals: {
+		// This is here because webpack tries to resolve a dev dependency which
+		// requires 'fs'
+		fs: 'fs'
+	}
+};
